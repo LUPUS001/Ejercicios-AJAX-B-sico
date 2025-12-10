fde882143b407900865baf8eb48cf1e4e2f20f9b
LUPUS001 <antoniodaw23@gmail.com>
2025-12-10 10:33:47 +0000

    Ejercicio 3 - Arreglado

--- Ej3/Ej3.js
+++ Ej3/Ej3.js
@@ -1,15 +1,11 @@
 var objeto = new XMLHttpRequest()
-var index = 0
 var imagen = document.getElementById("imagen")
 
-function sacardatos(){
-    objeto.open("GET", "imagen.txt", true);
+function sacardatos(datos){
+    objeto.open("GET", datos);
     objeto.onreadystatechange = function(){
         if (objeto.readyState == 4 && objeto.status == 200) {
-            var imagenes = objeto.responseText.trim().split("\n")
-            index = (index + 1) % imagenes.length
-
-            imagen.setAttribute("src", imagenes[index])
+            imagen.src = objeto.responseText
         }
     }
     objeto.send(null);
