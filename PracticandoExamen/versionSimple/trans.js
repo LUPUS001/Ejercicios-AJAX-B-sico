6fa7b217a3d2831be6cf35407dc31c05c937f18a
LUPUS001 <148451240+LUPUS001@users.noreply.github.com>
2026-02-06 08:42:24 +0100

    Correcciones

--- PracticandoExamen/versionSimple/trans.js
+++ PracticandoExamen/versionSimple/trans.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
         function() {
             // Ratón entra: se vuelve transparente (opacidad 0.2)
             // .stop() es vital para que no se vuelva loco si pasas el ratón rápido
-            $(this).stop().fadeTo(500, 0.2); 
+            $(this).fadeTo(500, 0); 
         }, 
         function() { 
             // Ratón sale: vuelve a verse bien (opacidad 1)
-            $(this).stop().fadeTo(500, 1.0); 
+            $(this).fadeTo(500, 1); 
         }
     );
 });
